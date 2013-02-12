67ec75f50d1d8b6c84ab9dfff2eda360a4897b29
Naveen Gupta <naveensky@gmail.com>
2013-02-12 20:06:21 +0530

    updated login filter for correct paths

--- trunk/application/routes.php
+++ trunk/application/routes.php
@@ -49,14 +49,12 @@ Route::Controller(Controller::detect());
 |
 */
 
-Event::listen('404', function()
-{
-	return Response::error('404');
+Event::listen('404', function () {
+    return Response::error('404');
 });
 
-Event::listen('500', function()
-{
-	return Response::error('500');
+Event::listen('500', function () {
+    return Response::error('500');
 });
 
 /*
@@ -87,23 +85,24 @@ Event::listen('500', function()
 |
 */
 
-Route::filter('before', function()
-{
-	// Do stuff before every request to your application...
+Route::filter('before', function () {
+    // Do stuff before every request to your application...
 });
 
-Route::filter('after', function($response)
-{
-	// Do stuff after every request to your application...
+Route::filter('after', function ($response) {
+    // Do stuff after every request to your application...
 });
 
-Route::filter('csrf', function()
-{
-	if (Request::forged()) return Response::error('500');
+Route::filter('csrf', function () {
+    if (Request::forged()) return Response::error('500');
 });
 
-Route::filter('auth', function()
-{
-	if (Auth::guest()) return Redirect::to('login');
+Route::filter('auth', function () {
+    if (Auth::guest()) {
+        if (Request::ajax())
+            return Redirect::to('/user/login');
+        else
+            return Redirect::to('/#/user/login');
+    }
 });
 
