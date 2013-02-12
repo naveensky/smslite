714d665a0abf617e668a051c57ae312bb6b92b03
Naveen Gupta <naveensky@gmail.com>
2013-02-12 11:14:38 +0530

    formatting issues

--- trunk/application/libraries/listenerconstants.php
+++ trunk/application/libraries/listenerconstants.php
@@ -8,12 +8,12 @@
  */
 class ListenerConstants
 {
-    const APP_USER_CREATED='app.user_created';
-    const APP_USER_DEACTIVATED='app.user_deactivated';
+    const APP_USER_CREATED = 'app.user_created';
+    const APP_USER_DEACTIVATED = 'app.user_deactivated';
     const APP_USER_RESTORE = 'app.user_restore';
-    const APP_USER_DELETE='app.user_delete';
-    const APP_USER_PASSWORD_FORGOT='app.user_forgot_password';
-    const APP_USER_PASSWORD_RESET='app.user_password_reset';
-    const APP_USER_PASSWORD_UPDATE='app.user_password_update';
+    const APP_USER_DELETE = 'app.user_delete';
+    const APP_USER_PASSWORD_FORGOT = 'app.user_forgot_password';
+    const APP_USER_PASSWORD_RESET = 'app.user_password_reset';
+    const APP_USER_PASSWORD_UPDATE = 'app.user_password_update';
     const MAXIMUM_MESSAGE_LENGTH = 306;
 }
