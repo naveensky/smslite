6bed2db2921a09816725cb5ad74c9f0a69afe78a
Naveen Gupta <naveensky@gmail.com>
2013-02-13 07:27:05 +0530

    added minify task to minify js and css for production use. need to work on this more

--- trunk/application/tasks/minify.php
+++ trunk/application/tasks/minify.php
@@ -7,6 +7,28 @@
  * To change this template use File | Settings | File Templates.
  */
 
-class Minify_Task {
+class Minify_Task
+{
 
+    public function run($arguments)
+    {
+        Bundle::start('compactor');
+
+        $compactor = new Compactor();
+
+        $cssPath = path('public') . "css/";
+        $jsPath = path('public') . "js/";
+        $jsControllers = path('public') . "js/controllers/";
+
+        $cssFiles = array(
+            $cssPath . 'bootstrap.min.css',
+            $cssPath . 'bootstrap-responsive.min.css',
+            $cssPath . 'fonts.min.css',
+            $cssPath . 'adminflare.min.css',
+            $cssPath . 'font-awesome.min.css',
+        );
+
+        $compactor->combine_files($cssFiles)->save_file($cssPath . "css-combine.css");
+        $compactor->combine_directory($jsControllers)->save_file($jsPath . "controllers.js");
+    }
 }
