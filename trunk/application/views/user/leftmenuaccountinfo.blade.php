158dcce361e8d798c59b43e7b902b154f5215137
Hitanshu <hitanshu@hitanshu-desktop.(none)>
2013-03-12 19:52:11 +0530

    permission changes to folder added view for account edit profile

---
+++ trunk/application/views/user/leftmenuaccountinfo.blade.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: hitanshu
+ * Date: 12/3/13
+ * Time: 5:47 PM
+ * To change this template use File | Settings | File Templates.
+ */
---
+++ trunk/public/js/controllers/user/account.js
@@ -0,0 +1,7 @@
+/**
+ * Created with JetBrains PhpStorm.
+ * User: hitanshu
+ * Date: 12/3/13
+ * Time: 7:28 PM
+ * To change this template use File | Settings | File Templates.
+ */
