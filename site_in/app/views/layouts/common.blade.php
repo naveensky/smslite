0256ed39659a5bc0cbca028074027cd0e8e2e2ee
Naveen Gupta <naveensky@gmail.com>
2013-03-15 10:49:02 +0530

    added google analytics code

--- site_in/app/views/layouts/common.blade.php
+++ site_in/app/views/layouts/common.blade.php
@@ -67,9 +67,25 @@
     </div>
 </div>
 
+
+<!--Google Analytics-->
+
+<script type="text/javascript">
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-16774060-27']);
+    _gaq.push(['_setDomainName', 'msngr.in']);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+
+</script>
+
 <!-- Le javascript
 ================================================== -->
-<!-- Placed at the end of the document so the pages load faster -->
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
 <script src="js/app.js"></script>
