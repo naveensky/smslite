ec81207153b2b128165d19e73c65abc23b261589
Naveen Gupta <naveensky@gmail.com>
2013-03-21 16:47:58 +0530

    added google analytics code

--- trunk/application/views/common/layout.blade.php
+++ trunk/application/views/common/layout.blade.php
@@ -27,6 +27,33 @@
 <!--scripts-->
 <% Asset::scripts() %>
 
+
+@if (Request::is_env("prod"))
+
+    <script type="text/javascript">
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', 'UA-16774060-27']);
+        _gaq.push(['_setDomainName', 'app.msngr.in']);
+        _gaq.push(['_trackPageview']);
+
+        (function () {
+            var ga = document.createElement('script');
+            ga.type = 'text/javascript';
+            ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0];
+            s.parentNode.insertBefore(ga, s);
+        })();
+
+        //track ajax requests
+        $(document).on('ajaxComplete', function (event, request, settings) {
+            _gaq.push(['_trackPageview', settings.url]);
+        });
+
+    </script>
+
+@endif
+
 </body>
 </html>
 
